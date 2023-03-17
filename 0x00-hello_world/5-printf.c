947c43e03e9023906d5c6cd112bd097e463dea2c
Mouhinf <mouhopap@gmail.com>
2023-03-16 18:37:17 -0700

    My first project C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-/*At the programm. I use printf*/
+/*
+ * At the programm. I use printf
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
