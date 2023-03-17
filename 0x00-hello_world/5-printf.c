dff63f6665ffe239dfc9300ba0d0003d3520289e
Mouhinf <mouhopap@gmail.com>
2023-03-16 17:53:06 -0700

    My first project C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-/*This is a programm of C*/
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+    printf("With proper grammar, but the outcome is a piece of art,\n");
+    return 0;
 }
+
