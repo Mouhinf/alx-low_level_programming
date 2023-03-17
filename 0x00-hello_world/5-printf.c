1dca0fe45825c22ceba67fc8ac1da12732022b72
Mouhinf <mouhopap@gmail.com>
2023-03-16 17:47:16 -0700

    My first project C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * The main function is the entry point of the program.
+ * It prints a message to the console and returns 0 to indicate successful completion.
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
