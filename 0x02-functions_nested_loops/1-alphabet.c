6d9226b7b86736b602d01957badc73b3e41eb7ea
Mouhinf <mouhopap@gmail.com>
2023-03-21 15:33:18 -0700

    My 3th project C

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,20 @@
-#include "main.h"
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
+#include <stdio.h>
+
+void print_alphabet(void);
+
+int main(void)
+{
+    print_alphabet();
+    return 0;
+}
+
 void print_alphabet(void)
 {
-    	char letter = 'a';
-
-    	while (letter <= 'z')
+    	char letter;
+    	for(letter = 'a'; letter <= 'z'; letter++)
     	{
-        	_putchar(letter);
-        	letter++;
+        	putchar(letter);
     	}
-
-    	_putchar('\n');
+	putchar('\n');
 }
 
