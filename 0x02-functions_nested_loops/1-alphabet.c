907a36a5e988aa21263c129bd90d023cf9df553b
Mouhinf <mouhopap@gmail.com>
2023-03-21 13:07:50 -0700

    My 3th project C

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_alphabet()
+{
+	printf("abcdefghijklmnopqrstuvwxyz\n");
+}
+int main(void)
+{
+	/**
+ 	 * main - check the code
+	 *
+	 * Return: Always 0.
+	 */
+	print_alphabet();
+	return (0);
+}
