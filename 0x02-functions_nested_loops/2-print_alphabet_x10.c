a07b164d225b80f3d814abc7780c4b52c475d488
Mouhinf <mouhopap@gmail.com>
2023-03-22 14:07:23 -0700

    My 3th project C

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,16 +4,17 @@
  *
  * Return: Always 0.
  */
-void print_alphabet_x10(void)	
+
+void print_alphabet_x10(void)
 {
 	int i = 1;
 	for (i = 1; i <= 10; i++)
 	{
 		char letter;
-	   	for (letter = 'a'; letter <= 'z'; letter++)
-       		{		 
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
 			_putchar(letter);
-       	 	}
-       		_putchar('\n');
+		}
+		_putchar('\n');
 	}
 }
