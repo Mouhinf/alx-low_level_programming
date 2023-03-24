747d615e8183bf3a8b08a56a80810aaff91bda07
Mouhinf <mouhopap@gmail.com>
2023-03-23 18:06:24 -0700

    My 4th project C

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void more_numbers(void)
+{
+	int j;
+	for (j = 0; j <= 10; j++)
+	{
+		int i;
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				_putchar((i/10)+'0');
+			}
+			_putchar((i%10) + '0');
+		}
+		_putchar('\n');
+	}
+}
