a6b1de7f9fa2e2cb231ee747cb59281a5cb4fc35
Mouhinf <mouhopap@gmail.com>
2023-03-28 16:26:34 -0700

    My 5th project C

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,10 +4,12 @@
  * swap_int - Swaps the values of two integers
  *
  * @a: Pointer to an integer
+ * @b: Pointer to an integer
  */
 void swap_int(int *a, int *b)
 {
 	int tmp;
+
 	tmp = *a;
 	*a = *b;
 	*b = tmp;
