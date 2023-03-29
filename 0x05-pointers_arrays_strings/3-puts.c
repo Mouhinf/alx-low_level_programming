27a0a54325dfa9ea88f331a76ba94c078b177156
Mouhinf <mouhopap@gmail.com>
2023-03-28 18:22:31 -0700

    My 5th project C

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,16 @@
 #include "main.h"
 
+/**
+ * _puts - prints a string to stdout, followed by a new line
+ * @str: string to be printed
+ *
+ * Return: void
+ */
 void _puts(char *str)
 {
 	if (*str != '\0')
 	{
-		puts(str);	
+		puts(str);
 	}
 	else
 	{
